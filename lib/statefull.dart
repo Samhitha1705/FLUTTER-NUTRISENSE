abd2be1ff492141597421c4338c9f25967132a9a
Ravitejaganji1234 <ravitejaganji49@gmail.com>
2026-02-03 20:02:09 +0530

    up

--- lib/statefull.dart
+++ lib/statefull.dart
@@ -101,7 +101,7 @@ class maniPage extends StatefulWidget {
 }
 
 class _maniPageState extends State<maniPage> {
-  int currentIndex = 1;
+  int currentIndex = 0;
 
   List dashboardScreens = [
     homePageScreen(),
